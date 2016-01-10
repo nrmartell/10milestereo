d6df4826726bfbefec90619cf043499ac543e17f
Natalie Martell <nrmartell@gmail.com>
2016-01-10 16:35:04 -0700

    password resets

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 	has_many :microposts
-	attr_accessor :remember_token, :activation_token
+	attr_accessor :remember_token, :activation_token, :reset_token
 	before_save :downcase_email
 	before_create :create_activation_digest
 	validates :name, presence: true, length: { maximum: 50 }
@@ -36,15 +36,6 @@ class User < ActiveRecord::Base
 		update_attribute(:remember_digest, nil)
 	end
 
-	def downcase_email
-		self.email = email.downcase
-	end
-
-	def create_activation_digest
-		self.activation_token = User.new_token
-		self.activation_digest= User.digest(activation_token)
-	end
-
 	def activate
 		update_attribute(:activated, true)
 		update_attribute(:activated_at, Time.zone.now)
@@ -54,6 +45,30 @@ class User < ActiveRecord::Base
 		UserMailer.account_activation(self).deliver_now
 	end
 
+	def create_reset_digest
+    self.reset_token = User.new_token
+    update_attribute(:reset_digest,  User.digest(reset_token))
+    update_attribute(:reset_sent_at, Time.zone.now)
+  	end
+
+  # Sends password reset email.
+  	def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  	end
+
+  	def password_reset_expired?
+  		reset_sent_at < 2.hours.ago
+  	end
+
 	private
+
+	def downcase_email
+		self.email = email.downcase
+	end
+
+	def create_activation_digest
+		self.activation_token = User.new_token
+		self.activation_digest= User.digest(activation_token)
+	end
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,21 @@ Rails.application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'boiling-journey-6524.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+
+
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
