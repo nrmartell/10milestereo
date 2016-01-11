8285eb253de75111b498fc10a99f52d2db78db28
Natalie Martell <nrmartell@gmail.com>
2016-01-10 23:18:33 -0700

    mailer issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -63,7 +63,7 @@ class User < ActiveRecord::Base
 
   # Sends password reset email.
   	def send_password_reset_email
-    UserMailer.password_reset(self).deliver_now
+    	UserMailer.password_reset(self).deliver_now
   	end
 
   	def password_reset_expired?
