9455c42b877492c80c1ed0c6cd6727f3a27c375f
Natalie Martell <nrmartell@gmail.com>
2016-01-11 19:51:45 -0700

    add following capability

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,38 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-# 99.times do |n|
-#   name  = Faker::Name.name
-#   email = "example-#{n+1}@tacoboutit.org"
-#   password = "password"
-#   User.create!(name:  name,
-#                email: email,
-#                password:              password,
-#                password_confirmation: password)
-# end
+# Users
+User.create!(name:  "Natalie",
+             email: "admin@tacoboutit.com",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin:     true,
+             activated: true,
+             activated_at: Time.zone.now)
 
-# users = User.order(:created_at).take(6)
-# 50.times do
-#   content = Faker::Lorem.sentence(5)
-#   users.each { |user| user.microposts.create!(content: content) }
-# end
+99.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n+1}@railstutorial.org"
+  password = "password"
+  User.create!(name: name,
+              email: email,
+              password:              password,
+              password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
+end
+
+# Microposts
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
+end
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
